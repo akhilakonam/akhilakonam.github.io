74750d4098330faaf01526698319b8bbb9f5726b
Akhila Konam <41164801+akhilakonam@users.noreply.github.com>
2021-11-21 21:32:13 +0530

    set up default route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import history from './history';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import LandingPage from './LandingPage/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+axios.defaults.withCredentials = true;
+
+export default function App(props) {
+    return (
+        <BrowserRouter history={history}>
+            <Routes>
+              <Route path='/' element={<LandingPage/>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class LandingPage extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                Web developer portfolio
+            </div>
+        )
+    }
+}
